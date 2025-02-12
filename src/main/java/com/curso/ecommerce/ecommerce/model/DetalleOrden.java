609f5a7f0c674c21b183d04cdbcf305111fb1d36
andrea <a.neira@pucp.edu.pe>
2025-02-12 14:13:29 -0500

    first commit - models creation

---
+++ src/main/java/com/curso/ecommerce/ecommerce/model/DetalleOrden.java
@@ -0,0 +1,71 @@
+package com.curso.ecommerce.ecommerce.model;
+
+public class DetalleOrden {
+    private int id;
+    private String nombre;
+    private double precio;
+    private double total;
+    private double cantidad;
+
+    public DetalleOrden() {
+    }
+
+    public DetalleOrden(int id, String nombre, double precio, double total, double cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.total = total;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleOrden{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", total=" + total +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+}
---
+++ src/main/java/com/curso/ecommerce/ecommerce/model/Producto.java
@@ -0,0 +1,82 @@
+package com.curso.ecommerce.ecommerce.model;
+
+public class Producto {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private Integer cantidad;
+    private Double precio;
+    private String imagen;
+
+    public Producto() {
+    }
+
+    public Producto(Integer id, String nombre, String descripcion, Integer cantidad, Double precio, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.imagen = imagen;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", cantidad=" + cantidad +
+                ", precio=" + precio +
+                ", imagen='" + imagen + '\'' +
+                '}';
+    }
+}
